d33423c837ea5872d7da315adcf4224bcd37aee3
ftomaz-c <fabiocastim@gmail.com>
2023-07-06 18:03:35 +0100

    6JUL1803

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,37 +6,26 @@
 /*   By: ftomaz-c <ftomaz-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 18:52:38 by ftomaz-c          #+#    #+#             */
-/*   Updated: 2023/07/05 23:57:23 by ftomaz-c         ###   ########.fr       */
+/*   Updated: 2023/07/06 18:00:57 by ftomaz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-
-#define BUFFER_SIZE 1024
+#include "get_next_line.h"
 
 int	main(void)
 {
 	int		fd;
-	char	buffer[BUFFER_SIZE];
-	ssize_t	bytes_read;
 
-	fd = open("hp1.txt", O_RDONLY);
+	fd = open("../hp1.txt", O_RDONLY);
 	if (fd == -1)
 	{
 		printf("\033[31mError opening the file.\033[0m\n");
 		return (1);
 	}
-	bytes_read = read(fd, buffer, BUFFER_SIZE-1);
-	if (bytes_read == -1)
-	{
-		printf("\033[31mError reading the file.\033[0m\n");
-		close(fd);
-		return (1);
-	}
-	buffer[bytes_read] = '\0';
-	printf("\033[34mFILE CONTENT:\033[0m\n%s\n", buffer);
+	get_next_line(fd);
 	close(fd);
 	return (0);
 }
